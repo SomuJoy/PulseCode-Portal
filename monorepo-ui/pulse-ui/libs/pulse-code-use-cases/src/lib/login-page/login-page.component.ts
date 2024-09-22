df7ba4d4a12df6dc2ebe6c3d3051ec987c85b6a1
SomuJoy24 <somu.joy24@gmail.com>
2024-09-22 14:53:32 +0530

    added libs

---
+++ monorepo-ui/pulse-ui/libs/pulse-code-use-cases/src/lib/login-page/login-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-login-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss',
+})
+export class loginPageComponent {}
